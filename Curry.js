983cad64e32ba2a76efed86ec5a8e5026f2296f8
ahmadhdr <ahmadhdr.22@gmail.com>
2019-11-11 01:54:54 +0700

    definition curry in javascript

---
+++ Curry.js
@@ -0,0 +1,7 @@
+/**
+ * Currying  adalah metode populer di kalangan javascript developer
+ * untuk membuat function dimana argument function tersebut
+ * di translate menjadi stand alone function
+ * example :
+ * function(a,b) ----> function(a)(b)
+ */
