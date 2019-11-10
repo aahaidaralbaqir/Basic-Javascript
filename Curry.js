9ead46cd7ef27a982085494b1b5590ff97711010
ahmadhdr <ahmadhdr.22@gmail.com>
2019-11-11 02:05:28 +0700

    how curried function work on javascript

--- Curry.js
+++ Curry.js
@@ -5,3 +5,34 @@
  * example :
  * function(a,b) ----> function(a)(b)
  */
+
+// di javascrip tkti sah-sah saja membuat fungsi begini
+function add() {
+    return function(a) {
+        return function(b){
+            return a + b
+        }
+    }
+}
+
+console.log(add()(2)(3))
+
+/**
+ * currying bisa berjalan karena javascript support closure ,
+ * tapi sabar dulu nih heheh,currying bisa jadi kurang flexible  karena  terlalu banyak nested di dalam function
+ */
+
+ function makeCurry(uncurried) {
+     let params = Array.prototype.slice.call(arguments,1)
+     return function () {
+         return uncurried.apply(this,params.concat(
+             Array.prototype.slice.call(arguments,0)
+         ))
+     }
+ }
+
+ let greeting = function(greet, title, education, name) {
+    return greet + title + education + name;
+  };
+ let greetHello2 = makeCurry(greeting, "Hello", "Mr.");
+ console.log(greetHello2('S.kom', 'Budi')); // Hello Mr.Budi S.kom
